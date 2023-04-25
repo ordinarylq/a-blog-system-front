4f32cc454489e443c68a09067ddcd7e0444bd482
Li Qi <973129554@qq.com>
2023-04-25 20:27:54 +0800

    feat: Add theme support to footer

--- src/app/common/footer/footer.component.ts
+++ src/app/common/footer/footer.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ThemeManagerService } from '../service/theme-manager.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnDestroy {
 
+  isDark = this.themeManager.isDark;
+
+  constructor(private themeManager: ThemeManagerService) {
+    this.themeManager.isDarkChange.subscribe(value => {
+      this.isDark = value;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.themeManager.isDarkChange.unsubscribe();
+  }
 }
