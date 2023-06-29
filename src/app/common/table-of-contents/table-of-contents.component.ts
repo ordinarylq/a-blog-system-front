a9b261405ac83e8ee54d328c564534bc362e74a5
Li Qi <973129554@qq.com>
2023-06-29 20:14:46 +0800

    feat: Add TOC scroll

--- src/app/common/table-of-contents/table-of-contents.component.ts
+++ src/app/common/table-of-contents/table-of-contents.component.ts
@@ -10,4 +10,19 @@ export class TableOfContentsComponent {
   @Input()
   headerElements: Element[] = [];
 
+  /**
+   * 根据TOC滑动到指定的标题位置
+   * 注意：需要排除掉顶部导航栏的高度
+   * @param headerId 标题ID
+   */
+  navigateToHeader(headerId: string) {
+    const headerElement = document.getElementById(headerId);
+    const navBarHeight = (document.getElementsByTagName('app-nav-bar')[0].childNodes[0] as HTMLElement).offsetHeight;
+    const scrollPosition = headerElement!.getBoundingClientRect().top + window.scrollY - navBarHeight;
+    window.scrollTo({
+      top: scrollPosition,
+      behavior: 'smooth'
+    })
+  }
+
 }
